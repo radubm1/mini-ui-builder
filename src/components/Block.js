34c05601965a6c49a7adb8bf546708471b70fd33
Radu Manea <radub_m@yahoo.com>
2025-11-19 11:48:59 +0200

    Fix: Am incarcat o pisica in Assets si Blocks

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import kitten from '../assets/kitten.jpg';
 
 export default function Block({ block, onRemove }) {
   return (
     <div className="block">
       {block.type === 'title' && <h1>{block.content}</h1>}
-      {block.type === 'image' && <img src={block.content} alt="bloc vizual" />}
+	  {block.type === 'image' && <img src={kitten} alt="pisică" />}
       {block.type === 'text' && <p>{block.content}</p>}
       {block.type === 'button' && <button>{block.content}</button>}
       <button className="remove" onClick={onRemove}>❌</button>
